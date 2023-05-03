ffe7dabdf1b0c080039e76fdbac9718cd3ea8fe3
clararitto <128714704+clararitto@users.noreply.github.com>
2023-05-03 20:10:28 -0300

    Add files via upload

---
+++ revisao.py
@@ -0,0 +1,60 @@
+#mostrar salario,idade e nome
+"""n1=input("Digite o seu nome: ")
+id=int(input("Digite sua idade: "))
+sal=float(input("Digite seu lalario:" ))
+print(n1,id,sal)"""
+#fazer as 4 operações
+"""n1=int(input("Digite um numero inteiro: "))
+n2=int(input("Digite um numero inteiro: "))
+soma=n1+n2
+multi=n1*n2
+div=n1//n2
+sub=n1-n2
+print(soma,multi,div,sub)"""
+#trocar valores
+"""a=5
+b=10
+x=0
+x=a
+a=b
+b=x
+print(a,b)"""
+#ordem crescente
+"""n1=int(input("Digite um numero inteiro: "))
+n2=int(input("Digite um numero inteiro: "))
+if n1>n2:
+    print(n2,n1)
+else:
+    print(n1,n2)"""
+# receber 3 notas de um aluno e falar se ta aprovado ou nao
+"""n1=float(input("Digite a 1º nota : "))
+n2=float(input("Digite a 2º nota : "))
+n3=float(input("Digite a 3º nota : "))
+media=(n1+n2+n3)/3
+if media>=7:
+    print("APROVADO")
+elif media>=4:
+    print("RECUPERAÇÃO")
+else:
+    print("REPROVADO")"""
+#PAR OU IMPAR
+"""n=int(input("Digite um numero inteiro: "))
+if n%2==0:
+    print("PAR")
+else:
+    print("IMPAR")"""
+#
+"""num=int(input("Digite um numero"))
+
+for x in range(1,11):
+    print(num*x)"""
+#mostre de 1 a 10
+"""for y in range(10,0,-1):
+    print(y)"""
+#imprimir os 10 primeiros numeros  maiores que 100
+"""for x in range(100,111,1):
+    print(x)"""
+#
+"""=int(input("Digite o um numero: "))
+for x in range(1,n+1,1):
+    print(x)"""
