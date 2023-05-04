7db52bad0ccabe0faddf340583fcb61d285b7560
Clara :) <128714704+clararitto@users.noreply.github.com>
2023-05-04 12:15:20 -0300

    Add files via upload

---
+++ revisão.py
@@ -0,0 +1,54 @@
+"""soma=0
+media=0
+for x in range(4):
+    num=int(input("digite um numero"))
+    soma+=num
+media=soma/4
+print(media)"""
+
+"""base=float(input("Digite a base do triangulo"))
+altura=float(input("Digite a altura do triangulo"))
+area=(base*altura)/2
+print(area)"""
+
+"""num=int(input("Digite um numero inteiro"))
+if num>0:
+    calcul = num-1
+    print(calcul)
+else:
+    cal= num+1
+    print(cal)"""
+
+"""resp="s"
+while resp=="s":
+    anos=int(input("Digite sua idade em anos"))
+    meses=int(input("Digite sua idade em meses"))
+    dias=int(input("Digite sua idade em dias"))
+    dia=365*anos
+    mes=30*meses
+    q=mes+dia+dias
+    print(q)
+    resp=input("deseja continuar?: s/n")"""
+
+"""eleitor_total=int(input("Digite o total de eleitores"))
+votos_brancos=int(input("Digite o numero de votos brancos"))
+votos_nulos=int(input("Digite o numero de votos nulos"))
+votos_validos=int(input("Digite o numero de votos validos"))
+percent_brancos=(votos_brancos/eleitor_total)*100
+percent_nulos=(votos_nulos/eleitor_total)*100
+percent_valid=(votos_validos/eleitor_total)*100
+print(percent_nulos,percent_brancos,percent_valid)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
